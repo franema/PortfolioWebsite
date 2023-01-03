05376d7592bf80a81d6fca20bd18bd586db2fdc0
DonCosme <109149406+franema@users.noreply.github.com>
2023-01-03 18:03:27 -0300

    Adds initial page

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import Home from "./Components/Home";
 import AboutMe from "./Components/AboutMe";
 import Projects from "./Components/Projects";
 import Contact from "./Components/Contact";
+import WelcomePage from "./Components/WelcomePage";
 
 function App() {
   return (
     <div className="App">
+      <WelcomePage></WelcomePage>
       <BrowserRouter>
         <Navbar></Navbar>
         <Routes>
