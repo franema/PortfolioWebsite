0bdc5204be90311f901b51303a9fd4f537f7b913
DonCosme <109149406+franema@users.noreply.github.com>
2023-01-03 16:17:31 -0300

    Creates Links in navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import "../CSS/Navbar.css"
+import { Link } from 'react-router-dom'
 function Navbar() {
     return (
         <div className='navbar_container'>
             <div className='navbar'>
-                <p>Link</p>
-                <p>Link</p>
-                <p>Link</p>
-                <p>Link</p>
+                <Link to="/">Home</Link>
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
             </div>
         </div>
     )
