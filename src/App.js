6674371b8f4f75c6933f5cb7ad0a0aee34134ab2
DonCosme <109149406+franema@users.noreply.github.com>
2023-01-03 09:31:22 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./Components/Navbar";
+import "./CSS/App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <div>Hola Miguel</div>
+    </div>
+  );
+}
+
+export default App;
