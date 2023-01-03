e64ab0290485556dc1666743485d5793026ded93
DonCosme <109149406+franema@users.noreply.github.com>
2023-01-03 16:19:35 -0300

    Creates React Router
    Allows to travel through all the pages of the site

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import "./CSS/App.css"
+import Home from "./Components/Home";
+import AboutMe from "./Components/AboutMe";
+import Projects from "./Components/Projects";
+import Contact from "./Components/Contact";
 
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
-      <div>Hola Miguel</div>
+      <BrowserRouter>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/aboutme" element={<AboutMe/>}/>
+          <Route path="/projects" element={<Projects/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
