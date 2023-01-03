c4474c9719443b6d7f9ce56b478adb539b3c623a
DonCosme <109149406+franema@users.noreply.github.com>
2023-01-03 09:31:25 -0300

    Create Navbar.js

---
+++ src/Components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../CSS/Navbar.css"
+function Navbar() {
+    return (
+        <div className='navbar_container'>
+            <div className='navbar'>
+                <p>Link</p>
+                <p>Link</p>
+                <p>Link</p>
+                <p>Link</p>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
