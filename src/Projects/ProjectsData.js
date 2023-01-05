dbd8f9ec886c903a4d945f502ddcf78423c15868
DonCosme <109149406+franema@users.noreply.github.com>
2023-01-05 17:19:48 -0300

    Cleans code

--- src/Projects/ProjectsData.js
+++ src/Projects/ProjectsData.js
@@ -7,12 +7,33 @@ import WelcomeEnglish from "../img/EnglishImg2.png"
 import Calculator from "../img/CalculatorImg2.png"
 
 export const ProjectsData = [
-    {name:"Sign Up Form", isLeftText: true, anchor: "https://franema.github.io/Sign-up_Form/", image: signUpImg},
-    {name:"Admin Dashboard", isLeftText: false, anchor: "https://franema.github.io/Admin-Dashboard/", image: DashboardImg},
-    {name:"Weather App", isLeftText: true, anchor: "https://franema.github.io/WeatherPage/", image: WeatherApp},
-    {name:"To Do List", isLeftText: false, anchor: "https://franema.github.io/ToDoList/", image: ToDoList},
-    {name:"Tic Tac Toe", isLeftText: true, anchor: "https://franema.github.io/Tic-Tac-Toe/", image: TicTacToe},
-    {name:"Welcome English", isLeftText: false, anchor: "https://22516-full-stack-python.github.io/-Proyecto-Welcomed-English-Bernal/", image: WelcomeEnglish},
-    {name:"Calculator", isLeftText: true, anchor: "https://franema.github.io/Calculator/", image: Calculator}
+    {name:"Sign Up Form",
+     isLeftText: true,
+     anchor: "https://franema.github.io/Sign-up_Form/",
+     image: signUpImg},
+    {name:"Admin Dashboard",
+     isLeftText: false,
+     anchor: "https://franema.github.io/Admin-Dashboard/",
+     image: DashboardImg},
+    {name:"Weather App",
+     isLeftText: true,
+     anchor: "https://franema.github.io/WeatherPage/",
+     image: WeatherApp},
+    {name:"To Do List",
+     isLeftText: false,
+     anchor: "https://franema.github.io/ToDoList/",
+     image: ToDoList},
+    {name:"Tic Tac Toe",
+     isLeftText: true,
+     anchor: "https://franema.github.io/Tic-Tac-Toe/",
+     image: TicTacToe},
+    {name:"Welcome English",
+     isLeftText: false, 
+     anchor: "https://22516-full-stack-python.github.io/-Proyecto-Welcomed-English-Bernal/", 
+     image: WelcomeEnglish},
+    {name:"Calculator",
+     isLeftText: true, 
+     anchor: "https://franema.github.io/Calculator/", 
+     image: Calculator}
 ]
 
