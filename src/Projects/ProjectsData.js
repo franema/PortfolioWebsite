d7d8a81e58e2b218549219f4aaaa0623877b54f1
DonCosme <109149406+franema@users.noreply.github.com>
2023-01-05 17:01:51 -0300

    Create ProjectsData.js

---
+++ src/Projects/ProjectsData.js
@@ -0,0 +1,18 @@
+import signUpImg from "../img/SignUpImg2.png"
+import DashboardImg from "../img/DashboardImg2.png"
+import WeatherApp from "../img/WeatherAppImg2.png"
+import ToDoList from "../img/ToDoListImg2.png"
+import TicTacToe from "../img/TicTacToeImg2.png"
+import WelcomeEnglish from "../img/EnglishImg2.png"
+import Calculator from "../img/CalculatorImg2.png"
+
+export const ProjectsData = [
+    {name:"Sign Up Form", isLeftText: true, anchor: "https://franema.github.io/Sign-up_Form/", image: signUpImg},
+    {name:"Admin Dashboard", isLeftText: false, anchor: "https://franema.github.io/Admin-Dashboard/", image: DashboardImg},
+    {name:"Weather App", isLeftText: true, anchor: "https://franema.github.io/WeatherPage/", image: WeatherApp},
+    {name:"To Do List", isLeftText: false, anchor: "https://franema.github.io/ToDoList/", image: ToDoList},
+    {name:"Tic Tac Toe", isLeftText: true, anchor: "https://franema.github.io/Tic-Tac-Toe/", image: TicTacToe},
+    {name:"Welcome English", isLeftText: false, anchor: "https://22516-full-stack-python.github.io/-Proyecto-Welcomed-English-Bernal/", image: WelcomeEnglish},
+    {name:"Calculator", isLeftText: true, anchor: "https://franema.github.io/Calculator/", image: Calculator}
+]
+
