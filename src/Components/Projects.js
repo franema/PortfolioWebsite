f5d053d82e016f3d6bc62b379f3dcc95303d3109
DonCosme <109149406+franema@users.noreply.github.com>
2023-01-11 09:32:36 -0300

    Adds keys

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "../CSS/Projects.css"
-import { ProjectsData } from "../Projects/ProjectsData"
+import { ProjectsData } from "../Data/ProjectsData"
 
 function Projects() {
 
@@ -8,7 +8,7 @@ function Projects() {
         <div className='projects-container'>
             {ProjectsData.map(project => (
                 project.isLeftText ? (
-                    <div className={"project"}>
+                    <div className={"project"} key={project.name}>
                         <div className='project_txt'>
                             <h4 className="left_text">{project.name}</h4>
                             <p className='left_text'>Algo y algo mas y no se alguna otra cosa, ya veremos... o no</p>
@@ -20,7 +20,7 @@ function Projects() {
                         </div>
                     </div>
                 ) : (
-                    <div className={"project"}>
+                    <div className={"project"} key={project.name}>
                         <div className="project_img">
                             <a href={project.anchor} target="_blank" rel="noopener noreferrer">
                                 <img src={project.image} alt="" />
