7cd1d385c62c89d35a3537987ad79c7edf81c95f
DonCosme <109149406+franema@users.noreply.github.com>
2023-01-11 09:31:55 -0300

    Adds "About" content

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Typewriter from "typewriter-effect"
+import "../CSS/About.css"
 
 function AboutMe() {
+
+  const [show, setShow] = useState("")
+
+  function showAbout () {
+    setShow("show")
+  }
+
   return (
-    <div>AboutMe</div>
+    <div className='home_container'>
+      <div className='home_message'>
+        <h3 className="title">
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+                .typeString("Francisco Marin")
+                .pause()
+                .callFunction(showAbout)
+                .start()
+            }}>
+          </Typewriter>
+        </h3>
+        <p className={`about_txt ${show}`}>Es.... Dinamicooooo y el más hermoso del universo</p>
+        <p className={`about_txt ${show}`}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam, autem.</p>
+        <p className={`about_txt ${show}`}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam, autem.</p>
+        <p className={`about_txt ${show}`}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam, autem.</p>
+      </div>
+    </div>
   )
 }
 
